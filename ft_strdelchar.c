51cabeefb508d8c46f134f7d982cc7f6f5b68ff6
Frederick MARINHO <frmarinh@e3r3p3.42.fr>
2015-12-08 21:42:50 +0100

    Bad include

--- ft_strdelchar.c
+++ ft_strdelchar.c
@@ -6,13 +6,12 @@
 /*   By: frmarinh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/08 04:23:48 by frmarinh          #+#    #+#             */
-/*   Updated: 2015/12/08 05:12:28 by frmarinh         ###   ########.fr       */
+/*   Updated: 2015/12/08 21:41:58 by frmarinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 static	char	*del_char(char *str, char c)
 {
